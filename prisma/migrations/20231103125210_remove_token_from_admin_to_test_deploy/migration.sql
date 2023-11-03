3c82f444772a69b1ffa92cb93303d8eac5f906b9
Bouchemel-Nasreddine <bouchemelnasreddine@gmail.com>
2023-11-03 13:52:34 +0100

    test deploy prisma

---
+++ prisma/migrations/20231103125210_remove_token_from_admin_to_test_deploy/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `token` on the `Admin` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Admin" DROP COLUMN "token";
